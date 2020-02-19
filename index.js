11904e8cc79183abc1455c5f04f0acfb6ff640cb
yusuke higasa <higasa@studio-higasa.com>
2020-02-19 17:23:35 +0900

    Add dotenv to the project

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require("dotenv").config()
+
 const express = require("express")
 const path = require("path")
 const port = process.env.PORT || 5000
@@ -7,5 +9,6 @@ express()
   .use(express.json())
   .use(express.static(path.join(__dirname, "public")))
   .set("views", path.join(__dirname, "views"))
+  .get("/g/", (req, res) => res.json({ message: `Welcome to ${process.env.SERVICE_NAME}` }))
   .post("/hook/", (req, res) => res.json({ message: "hook" }))
   .listen(port, () => console.log(`Listening on ${ port }`))
